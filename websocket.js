5be13de039e8440adc5522ef7e6e2e144a64ff71
DESKTOP-6TDH965\astas <astas2286@gmail.com>
2024-08-05 16:54:07 +0200

    no message

--- websocket.js
+++ websocket.js
@@ -5,7 +5,7 @@ let io;
 function init(server) {
   io = new Server(server, {
     cors: {
-      origin: "http://localhost:3000",
+      origin: ["http://localhost:3000", "*"],
       methods: ["GET", "POST"]
     }
   });
